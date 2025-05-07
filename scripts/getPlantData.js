d833a7061ac48fe20594433dd12abad8676eeef1
Niels Aling <niels.aling@hva.nl>
2025-05-07 16:23:04 +0200

    material functions done

--- scripts/getPlantData.js
+++ scripts/getPlantData.js
@@ -1,140 +1,222 @@
 async function getGroups() {
-    const response = await fetch("data.json");
-    const data = await response.json();
-    console.log(data);
+	const response = await fetch("data.json");
+	const data = await response.json();
+	console.log(data);
 
-    // Remove radio buttons
-    const plantGroupSection = document.getElementById("plantGroup");
-    plantGroupSection.innerHTML = "";
-    
-    // Create radio button for each group
-    data.forEach((group, index) => {
-        const div = document.createElement("div");
+	// Remove radio buttons
+	const plantGroupSection = document.getElementById("plantGroup");
+	plantGroupSection.innerHTML = "";
 
-        const input = document.createElement("input");
-        input.setAttribute("type", "radio");
-        input.setAttribute("name", "group");
-        input.setAttribute("id", group.id);
-        input.setAttribute("value", group.group);
-        
-        // Only the first should be checked by default
-        if (index === 0) {
-            input.setAttribute("checked", "checked");
-        }
-        
-        const label = document.createElement("label");
-        label.setAttribute("for", group.id);
-        label.innerText = group.group;
-        
-        div.appendChild(input);
-        div.appendChild(label);
-        plantGroupSection.appendChild(div);
-    });
-    
-    // Call updatePlantTypes function to update plant types
-    updatePlantTypes(data);
+	// Create radio button for each group
+	data.forEach((group, index) => {
+		const div = document.createElement("div");
 
-    // Call updateMaterial function to update plant materials
-    updateMaterial(data);
-    
-    // When selection changes update plant types and materials
-    const groupRadios = document.querySelectorAll('input[name="group"]');
-    groupRadios.forEach(radio => {
-        radio.addEventListener('change', () => {
-            updatePlantTypes(data);
-            updateMaterial(data);
-        });
-    });
+		const input = document.createElement("input");
+		input.setAttribute("type", "radio");
+		input.setAttribute("name", "group");
+		input.setAttribute("id", group.id);
+		input.setAttribute("value", group.group);
+
+		// Only the first should be checked by default
+		if (index === 0) {
+			input.setAttribute("checked", "checked");
+		}
+
+		const label = document.createElement("label");
+		label.setAttribute("for", group.id);
+		label.innerText = group.group;
+
+		div.appendChild(input);
+		div.appendChild(label);
+		plantGroupSection.appendChild(div);
+	});
+
+	// Call updatePlantTypes function to update plant types
+	updatePlantTypes(data);
+
+	// Call updateMaterial function to update plant materials
+	updateMaterial(data);
+
+	// When selection changes update plant types and materials
+	const groupRadios = document.querySelectorAll('input[name="group"]');
+	groupRadios.forEach((radio) => {
+		radio.addEventListener("change", () => {
+			updatePlantTypes(data);
+			updateMaterial(data);
+		});
+	});
 }
 
 function updatePlantTypes(data) {
-    // Get selected group
-    const selectedGroup = document.querySelector('input[name="group"]:checked').value;
-    console.log("Selected group:", selectedGroup);
-    
-    // Remove radio buttons
-    const plantTypeSection = document.getElementById("plantType");
-    plantTypeSection.innerHTML = "";
-    
-    // Find the selected group
-    let plantGroup = data.find((g) => g.group == selectedGroup);
-    
-    // If the group is found, find the types
-    if (plantGroup && plantGroup.type) {
-        console.log("Plant types for", selectedGroup, ":", plantGroup.type);
-        
-        // Create radio buttons for each type in the selected group
-        plantGroup.type.forEach((type, index) => {
-            const div = document.createElement("div");
-            
-            const input = document.createElement("input");
-            input.setAttribute("type", "radio");
-            input.setAttribute("name", "type");
-            input.setAttribute("id", type.id);
-            input.setAttribute("value", type.name);
-            
-            // Only set the first one checked by default
-            if (index === 0) {
-                input.setAttribute("checked", "checked");
-            }
-            
-            const label = document.createElement("label");
-            label.setAttribute("for", type.id);
-            label.innerText = type.name;
-            
-            div.appendChild(input);
-            div.appendChild(label);
-            plantTypeSection.appendChild(div);
-        });
-    } else {
-        console.error("Could not find plant group or types for:", selectedGroup);
-    }
+	// Get selected group
+	const selectedGroup = document.querySelector(
+		'input[name="group"]:checked'
+	).value;
+	console.log("Selected group:", selectedGroup);
+
+	// Remove radio buttons
+	const plantTypeSection = document.getElementById("plantType");
+	plantTypeSection.innerHTML = "";
+
+	// Find the selected group
+	let plantGroup = data.find((g) => g.group == selectedGroup);
+
+	// If the group is found, find the types
+	if (plantGroup && plantGroup.type) {
+		console.log("Plant types for", selectedGroup, ":", plantGroup.type);
+
+		// Create radio buttons for each type in the selected group
+		plantGroup.type.forEach((type, index) => {
+			const div = document.createElement("div");
+
+			const input = document.createElement("input");
+			input.setAttribute("type", "radio");
+			input.setAttribute("name", "type");
+			input.setAttribute("id", type.id);
+			input.setAttribute("value", type.name);
+
+			// Only set the first one checked by default
+			if (index === 0) {
+				input.setAttribute("checked", "checked");
+			}
+
+			const label = document.createElement("label");
+			label.setAttribute("for", type.id);
+			label.innerText = type.name;
+
+			div.appendChild(input);
+			div.appendChild(label);
+			plantTypeSection.appendChild(div);
+		});
+	} else {
+		console.error(
+			"Could not find plant group or types for:",
+			selectedGroup
+		);
+	}
 }
 
-function updateMaterial(data){
-      // Get selected group
-      const selectedGroup = document.querySelector('input[name="group"]:checked').value;
-      console.log("Selected group:", selectedGroup);
+function updateMaterial(data) {
+	// Get selected group
+	const selectedGroup = document.querySelector(
+		'input[name="group"]:checked'
+	).value;
+	console.log("Selected group:", selectedGroup);
 
-    // Remove radio buttons
-    const plantMaterialSection = document.getElementById("plantMaterial");
-    plantMaterialSection.innerHTML = "";
+	// Remove radio buttons
+	const plantMaterialSection = document.getElementById("plantMaterial");
+	plantMaterialSection.innerHTML = "";
+
+	// Find the selected group
+	let plantGroup = data.find((g) => g.group == selectedGroup);
+
+	// If the group is found, find the types
+	if (plantGroup && plantGroup.material) {
+		console.log(
+			"Plant materials for",
+			selectedGroup,
+			":",
+			plantGroup.material
+		);
+
+		// Create radio buttons for each material in the selected group
+		plantGroup.material.forEach((material, index) => {
+			const div = document.createElement("div");
+
+			const input = document.createElement("input");
+			input.setAttribute("type", "radio");
+			input.setAttribute("name", "size");
+			input.setAttribute("id", material.size);
+			input.setAttribute("value", material.size);
+
+			// Only set the first one checked by default
+			if (index === 1) {
+				input.setAttribute("checked", "checked");
+			}
+
+			const label = document.createElement("label");
+			label.setAttribute("for", material.size);
+			label.innerText = material.size;
+
+			div.appendChild(input);
+			div.appendChild(label);
+			plantMaterialSection.appendChild(div);
+		});
+	} else {
+		console.error(
+			"Could not find plant group or types for:",
+			selectedGroup
+		);
+	}
+
+	//check which material is selected and put in css variable
+	function getSelectedMaterial() {
+		const selectedMaterial = document.querySelector(
+			'input[name="size"]:checked'
+		).value;
+		console.log("Selected material:", selectedMaterial);
+
+		// Set the CSS variable for the selected material
+		document.documentElement.style.setProperty(
+			"--material-size",
+			`"` + selectedMaterial + `"`
+		);
+
+		//product is material size * 0.1 (from liters to ml)
+		const productAmount = parseFloat(selectedMaterial) * 10;
+
+		//set css variable for product amount
+		document.documentElement.style.setProperty(
+			"--product-size",
+			`"` + productAmount + `ml"`
+		);
+	}
+
+	console.log("Capacity checkbox is unchecked");
+	// When selection changes update plant material sizes
+	const materialRadios = document.querySelectorAll('input[name="size"]');
+	materialRadios.forEach((radio) => {
+		radio.addEventListener("change", () => {
+			getSelectedMaterial();
+		});
+	});
+
+	const capacityCheckbox = document.querySelector("#capacity");
+
+	capacityCheckbox.addEventListener("change", () => {
+		if (capacityCheckbox.checked) {
+			console.log("Capacity checkbox is checked");
+			//get amount that is put in the input field
+			const capacityInput = document.querySelector("#ownCapacity");
+
+			capacityInput.addEventListener("input", () => {
+				const capacityValue = capacityInput.value;
+				console.log("Capacity value:", capacityValue);
+
+                //if value is empty set it to 1
+                if (capacityValue === "") {
+                    capacityValue = 1;
+                } else {
+                    // Set the CSS variable for the selected material
+                    document.documentElement.style.setProperty(
+                        "--material-size",
+                        `"` + capacityValue + `L"`
+                    );
     
-    // Find the selected group
-    let plantGroup = data.find((g) => g.group == selectedGroup);
+                    //product is material size * 0.1 (from liters to ml)
+                    const productAmount = parseFloat(capacityValue) * 10;
     
-    // If the group is found, find the types
-    if (plantGroup && plantGroup.material) {
-        console.log("Plant materials for", selectedGroup, ":", plantGroup.material);
-
-        
-        // Create radio buttons for each material in the selected group
-        plantGroup.material.forEach((material, index) => {
-            const div = document.createElement("div");
-            
-            const input = document.createElement("input");
-            input.setAttribute("type", "radio");
-            input.setAttribute("name", "size");
-            input.setAttribute("id", material.size);
-            input.setAttribute("value", material.size);
-            
-            // Only set the first one checked by default
-            if (index === 0) {
-                input.setAttribute("checked", "checked");
-            }
-            
-            const label = document.createElement("label");
-            label.setAttribute("for", material.size);
-            label.innerText = material.size;
-            
-            div.appendChild(input);
-            div.appendChild(label);
-            plantMaterialSection.appendChild(div);
-        });
-    } else {
-        console.error("Could not find plant group or types for:", selectedGroup);
-    }
-
+                    //set css variable for product amount
+                    document.documentElement.style.setProperty(
+                        "--product-size",
+                        `"` + productAmount + `ml"`
+                    );
+                }
+			});
+		} else {
+			getSelectedMaterial();
+		}
+	});
 }
 
 getGroups();
