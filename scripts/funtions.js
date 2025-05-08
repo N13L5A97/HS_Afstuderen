3c15534823d2db2396e90a5f266eef99906b4b33
Niels Aling <niels.aling@hva.nl>
2025-05-08 09:02:54 +0200

    decimals function

---
+++ scripts/funtions.js
@@ -0,0 +1,11 @@
+function validateDecimals(input) {
+    if (input.value === '' || input.value === '-') return;
+    
+    const regex = /^-?\d+(\.\d{0,2})?$/;
+    
+    if (!regex.test(input.value)) {
+        input.value = input.dataset.lastValid || '';
+    } else {
+        input.dataset.lastValid = input.value;
+    }
+}
